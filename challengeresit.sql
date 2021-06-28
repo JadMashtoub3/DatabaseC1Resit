6efc23c126520d958e5f3bf89d899d4ab7e69e43
JAD MASHTOUB <103596586@student.swin.edu.au>
2021-06-29 09:59:25 +1000

    tas 1 done

--- challengeresit.sql
+++ challengeresit.sql
@@ -1,1 +1,21 @@
 /*Jad Mashtoub 103596586
+
+Subject(SubjCode, Description)
+PK(SubjCode)
+
+Student(StudentID, Surname, GivenName, Gender)
+PK(StudentID)
+
+SubjectOffering(SubjCode, Year, Semester, Fee, StaffID)
+CompPK(SubjCode, Year, Semester)
+FK(StaffID) REF Teacher
+FK(SubjCode)REF subject
+
+Enrolment(StudentId, SubjCode, Year, Semester, Grade, DateEnrolled)
+CompPK(StudentId, SubjCode, Year, Semester)
+FK(StudentID) REF Student
+CompFK(SubjCode, Year, Semester) REF SubjectOffering
+
+Teacher(StaffID, Surname, GivenName)
+PK(StaffID)
+
